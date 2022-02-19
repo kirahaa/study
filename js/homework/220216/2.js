c675db7f29f7946a316e71512083b3e7c3022efb
김하영 <khy4018@gmail.com>
2022-02-19 13:04:55 +0900

    homework 2번 다른 풀이

--- js/homework/220216/2.js
+++ js/homework/220216/2.js
@@ -11,3 +11,15 @@ function countWord(sentence) {
 // countWord("The Curious Case of Benjamin Button");
 // countWord(" The first character is a blank");
 countWord("The last character is a blank ");
+
+function countWord2(input) {
+   if(input.length > 1000000) {
+       console.log('길이가 1,000,000 이하의 문자를 입력하세요');
+       return;
+   } else {
+       console.log(
+           [...new Set(input.split(' '))].filter(item => item !== '').length
+       );
+   }
+}
+countWord2('asd asd ddd sa');
