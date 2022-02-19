cf3152a56d8cdec194e131e861a87bb3bdbceb0e
김하영 <khy4018@gmail.com>
2022-02-19 15:24:51 +0900

    modify homework 1번

--- js/homework/220223/1.js
+++ js/homework/220223/1.js
@@ -3,7 +3,9 @@ function sortNumber(...arr) {
     for(let i = 1; i < arr.length; i++) {
         num.push(arr[i]);
     }
-    let sortArr = num.sort();
+    let sortArr = num.sort(function (a, b) {
+        return a - b;
+    });
     console.log(sortArr);
 }
-sortNumber(5, 10, 23, 14, 24, 64);
+sortNumber(5, 110, 23, 14, 24, 64);
