1dbfe99cd06c8d17326cd6f5d6bded1f09ab8bba
김하영 <khy4018@gmail.com>
2022-02-20 14:46:57 +0900

    #4.1 Minify + Autoprefixer

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 ## 1. 모던 자바스크립트 입문
 
+## 2. Gulp - Nomad Coders
+
--- gulp/gulpfile.babel.js
+++ gulp/gulpfile.babel.js
@@ -1,11 +1,29 @@
 import gulp from "gulp";
 import gpug from "gulp-pug";
 import del from "del";
+import ws from "gulp-webserver";
+import image from "gulp-image";
+import dartSass from 'sass';
+import gulpSass from 'gulp-sass';
+import autoprefixer from "gulp-autoprefixer";
+import miniCSS from "gulp-csso";
+
+const sass = gulpSass(dartSass);
 
 const routes = {
     pug: {
+        watch: "src/**/*.pug",
         src: "src/*.pug",
         dest: "build"
+    },
+    img: {
+        src: "src/img/*",
+        dest: "build/img"
+    },
+    scss: {
+        watch: "src/scss/**/*.scss",
+        src: "src/scss/style.scss",
+        dest: "build/css"
     }
 }
 
@@ -17,8 +35,37 @@ const pug = () =>
 
 const clean = () => del(["build"]);
 
-const prepare = gulp.dest([clean]);
+const webserver = () => gulp.src("build").pipe(ws({
+    livereload: true,
+    open: true
+}));
 
-const assets = gulp.series([pug]);
+const img = () =>
+    gulp
+        .src(routes.img.src)
+        .pipe(image())
+        .pipe(gulp.dest(routes.img.dest));
 
-export const dev = gulp.series([prepare, assets]);
+const styles = () =>
+    gulp
+        .src(routes.scss.src)
+        .pipe(sass().on("error", sass.logError))
+        .pipe(autoprefixer())
+        .pipe(miniCSS())
+        .pipe(gulp.dest(routes.scss.dest));
+
+const watch = () => {
+    gulp.watch(routes.pug.watch, pug);
+    gulp.watch(routes.img.src, img);
+    gulp.watch(routes.scss.watch, styles);
+}
+
+const prepare = gulp.series([clean, img]);
+
+const assets = gulp.parallel([pug, styles]);
+
+const postDev = gulp.parallel([webserver, watch]);
+
+export const dev = gulp.series([prepare, assets, postDev]);
+
+// series parallel 차이?
